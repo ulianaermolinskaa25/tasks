210a5a5dcb90ad377bd839f96bad85862f8a985f
ulianaermolinskaa25 <ulianaermolinskaa25@gmail.com>
2023-10-12 00:44:58 +0300

    edid exception handling

--- AEROFLOT/aeroflot.h
+++ AEROFLOT/aeroflot.h
@@ -10,17 +10,27 @@ class AEROFLOT {
 public:
     string destination;
     int flight_number;
-    string type_of_plane;
+    string aircraft_type;
 
-    void input_info() {
+    int * input_info() {
         cout << "Enter destination: " << endl;
         cin >> destination;
 
         cout << "Enter flight number: " << endl;
         cin >> flight_number;
 
-        cout << "Enter aircraft type: " << endl;
-        cin >> type_of_plane;
+        if (flight_number <= 0) {
+            cout << "Fligt number must be greater than 0!" << endl;
+        }
+
+        cout << "Enter aircraft type (BOEING or Airbus): " << endl;
+        cin >> aircraft_type;
+
+        if (aircraft_type != "BOEING" && aircraft_type != "Airbus") {
+            cout << "Plane not found." << endl;
+            return 0;
+        }
+        
     }
 
     friend void sort(AEROFLOT* array, int N);
